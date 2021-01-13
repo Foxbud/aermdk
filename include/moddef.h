d59121c0b4766753f3180dc4d522baf36e0e66e4
Foxbud <breadboardfox@gmail.com>
2021-01-13 12:49:20 -0500

    Initial commit.

---
+++ include/moddef.h
@@ -0,0 +1,10 @@
+#ifndef MODDEF_H
+#define MODDEF_H
+
+#include "aer/mod.h"
+
+/* ----- PUBLIC FUNCTIONS ----- */
+
+AERModDef DefineMod(void);
+
+#endif /* MODDEF_H */
---
+++ src/moddef.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+
+#include "export.h"
+
+/* ----- PUBLIC FUNCTIONS ----- */
+
+MOD_EXPORT AERModDef DefineMod(void) {
+  return (AERModDef){.constructor = NULL,
+                     .destructor = NULL,
+                     .registerSprites = NULL,
+                     .registerObjects = NULL,
+                     .registerObjectListeners = NULL,
+                     .roomStepListener = NULL,
+                     .roomChangeListener = NULL};
+}
